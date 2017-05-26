00bb4a1acbde99a7347e59c8d6281afdd19ddf5b
Chitroopa Manikkavasagam <chitroopa72193@gmail.com>
2017-05-26 10:55:33 -0700

    Display search results

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -3,9 +3,25 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayResult = function(searchResult) {
   $('#output').text("");
+  var acceptsNewPatientsFormat;
+
   for(var j=0;j<searchResult.length;j++){
-    $('#output').append('<h4>'+ searchResult[j]['firstName'] +" "+ searchResult[j]['lastName'] +'</h4>');
-    $('#output').append('<p><span class="result-title">Specialties: </span>'+ searchResult[j]['specialtyName'] +'</p>');
+    if(searchResult[j].acceptsNewPatients == true)
+    {
+      acceptsNewPatientsFormat = 'Yes';
+    }
+    else
+    {
+      acceptsNewPatientsFormat = 'No';
+    }
+    $('#output').append('<h4>'+ searchResult[j].firstName +" "+ searchResult[j].lastName +'</h4>'+
+    '<p><span class="result-title">Accept New Patients: </span>'+ acceptsNewPatientsFormat +'</p>'+
+    '<p><span class="result-title">Specialties: </span>'+ searchResult[j].specialtyName +'</p>'+
+    '<p><span class="result-title">Description: </span>'+ searchResult[j].specialtyDesc +'</p>'+
+    '<p><span class="result-title">Phone number: </span>'+ searchResult[j].phone +'</p>'+
+    '<p><span class="result-title">Address: </span>'+ '</p>'+
+    '<p>'+ searchResult[j].street +'</p>'+
+    '<p>'+ searchResult[j].city +','+ searchResult[j].state+'-'+ searchResult[j].zipCode +'</p>');
   }
 };
 
