8103b0be769329b79d2c888911c34f4219d71502
Chitroopa Manikkavasagam <chitroopa72193@gmail.com>
2017-05-26 15:17:33 -0700

    fix error for specialties not available

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,11 +1,14 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
+var clearResult = function() {
+  $('#output').text("");
+};
 
-var displayResult = function(searchResult) {
+var displayResult = function(searchResult, city) {
   $('#output').text("");
   var acceptsNewPatientsFormat;
 
-
+  $('#output').append('<h3>Below doctors are availabe near location '+city+'</h3>');
   for(var j=0;j<searchResult.length;j++){
     var doctorAddress = "";
     if(searchResult[j].acceptsNewPatients === true)
@@ -22,15 +25,16 @@ var displayResult = function(searchResult) {
     }
 
     $('#output').append(
-      '<div class="col-md-5 card">'+
+      '<div class="col-md-10 card">'+
         '<div class="card-header">'+
           '<h1>'+ searchResult[j].firstName +" "+ searchResult[j].lastName +'</h1>'+
           '<p>'+ searchResult[j].specialtyName +'</p>'+
+          '<p class="card-text">'+ searchResult[j].specialtyDesc +'<p>'+
+          '<img src="'+ searchResult[j].img+ '" />'+
         '</div>'+
         '<div class="card-block">'+
-          '<p class="card-text">'+ searchResult[j].specialtyDesc +'<p>'+
+          '<p><span class="result-title">Bio: </span>'+ searchResult[j].bio +'</p>'+
           '<p><span class="result-title">Accept New Patients: </span>'+ acceptsNewPatientsFormat +'</p>'+
-          '<p><span class="result-title">Phone number: </span>'+ searchResult[j].phone +'</p>'+
           '<h4 class="card-title">Address</h4>'+
           '<p>'+ doctorAddress +'</p>'+
         '</div>'+
@@ -42,9 +46,12 @@ var displayResult = function(searchResult) {
 $('document').ready(function(){
   $('#form-one').submit(function(event){
     var userInputMedicalIssue = $('#medical-issue').val();
+    var userCity = $('#city').val();
     $('#medical-issue').val("");
+    $('#city').val("");
     var newDoctorLookup = new Doctor();
-    newDoctorLookup.getDoctors(userInputMedicalIssue, displayResult);
+    clearResult();
+    newDoctorLookup.getDoctors(userInputMedicalIssue,userCity,displayResult);
     event.preventDefault();
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,53 +1,78 @@
 var apiKey = require('./../.env').apiKey;
+var googleApiKey = require('./../.env').googleApiKey;
 
 function Doctor() {
 
 }
 
-Doctor.prototype.getDoctors = function(medicalIssue, displayResult) {
+Doctor.prototype.getDoctors = function(medicalIssue, city, displayResult, clearResult) {
   var doctorDetails = {};
   var searchResult = [];
+  var latitude, longitude, searchCity;
 
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=47.606%2C-122.332%2C%2010&user_location=47.606%2C-122.332&skip=0&limit=20&user_key=' + apiKey)
- .then(function(response) {
-    var resultCount = response.data.length;
-    if (resultCount > 0)
-    {
+  $.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=+in+'+ city +'&key=' + googleApiKey)
+  .then(function(response) {
 
-      for(var i=0; i<resultCount; i++)
+    latitude = parseFloat((response.results[0].geometry.location.lat).toFixed(3));
+    longitude = parseFloat((response.results[0].geometry.location.lng).toFixed(3));
+    searchCity = response.results[0].formatted_address;
+    console.log(searchCity);
+
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location='+latitude+'%2C%20'+longitude+'%2C%2010&user_location='+ latitude+'%2C%20'+longitude+'&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(response) {
+     console.log(response);
+      var resultCount = response.data.length;
+      if (resultCount > 0)
       {
-        var addressArray = [];
-        var addressformat ="";
-        var addressCount = response.data[i].practices.length;
-        if(addressCount > 0)
+        for(var i=0; i<resultCount; i++)
         {
-          for(var k=0; k<addressCount; k++) {
-            addressformat = (response.data[i].practices[k].visit_address.street + ", " + response.data[i].practices[k].visit_address.city + ", " + response.data[i].practices[k].visit_address.state + "-" + response.data[i].practices[k].visit_address.zip);
+          var addressArray = [];
+          var addressformat ="";
+          var specialty, description;
+          var addressCount = response.data[i].practices.length;
+          if(addressCount > 0)
+          {
+            for(var k=0; k<addressCount; k++) {
+              addressformat = (response.data[i].practices[k].visit_address.street + ", " + response.data[i].practices[k].visit_address.city + ", " + response.data[i].practices[k].visit_address.state + "-" + response.data[i].practices[k].visit_address.zip);
+              addressArray.push(addressformat);
+            }
+          }
+          else
+          {
+            addressformat = (response.data[i].practices[0].visit_address.street + ", " + response.data[i].practices[0].visit_address.city + ", " + response.data[i].practices[0].visit_address.state + "-" + response.data[i].practices[0].visit_address.zip);
             addressArray.push(addressformat);
           }
+          if (response.data[i].specialties === undefined)
+          {
+            console.log(response.data[i]);
+            specialty = 'General';
+            description = 'General';
+          }
+          else
+          {
+            specialty = response.data[i].specialties[0].actor;
+            description = response.data[i].specialties[0].description;
+          }
+          var addressArraysorted = Array.from(new Set(addressArray));
+
+          doctorDetails = {firstName: response.data[i].profile.first_name,lastName: response.data[i].profile.last_name,specialtyName: specialty,specialtyDesc: description,acceptsNewPatients: response.data[i].practices[0].accepts_new_patients,address: addressArraysorted, img: response.data[i].profile.image_url, bio: response.data[0].profile.bio };
+          console.log(doctorDetails);
+          searchResult.push(doctorDetails);
+          doctorDetails = {};
         }
-        else
-        {
-          addressformat = (response.data[i].practices[0].visit_address.street + ", " + response.data[i].practices[0].visit_address.city + ", " + response.data[i].practices[0].visit_address.state + "-" + response.data[i].practices[0].visit_address.zip);
-          addressArray.push(addressformat);
-        }
-        var addressArraysorted = Array.from(new Set(addressArray));
-        // console.log(addressArray);
-        // console.log(addressArraysorted);
-        doctorDetails = {firstName: response.data[i].profile.first_name,lastName: response.data[i].profile.last_name,specialtyName: response.data[i].specialties[0].actor,specialtyDesc: response.data[i].specialties[0].description,acceptsNewPatients: response.data[i].practices[0].accepts_new_patients,phone: response.data[i].practices[0].phones[0].number,address: addressArraysorted};
-        searchResult.push(doctorDetails);
-        doctorDetails = {};
+        displayResult(searchResult, searchCity);
       }
-      // console.log(searchResult);
-      // console.log(searchResult.length);
-      displayResult(searchResult);
-    }
-    else
-    {
-      $('#output').text("No results found, try with different key word");
-    }
+      else
+      {
+        $('#output').text("No results found, try with different key word");
+      }
+    })
+   .fail(function(error){
+     debugger;
+      $('#output').text(error.responseJSON.message);
+    });
   })
- .fail(function(error){
+  .fail(function(error){
     $('#output').text(error.responseJSON.message);
   });
 };
